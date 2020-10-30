4aff998ab776b61c129062d35b29fb1564592fad
Harry Pan <hpan10@stuy.edu>
2020-10-30 16:49:24 -0400

    I've tested everything and it works!

--- SuperArray.java
+++ SuperArray.java
@@ -1,7 +1,7 @@
 public class SuperArray{
 
   //instance variables
-  private String[] data;
+  public String[] data;
   private int size;
 
   //constructor
@@ -16,14 +16,30 @@ public class SuperArray{
   }
 
   public boolean add(String element){
-    data[size] = element;
-    size++;
-    return true;
+    if (size == data.length) resize();
+    data[size] = element; size++; return true;
   }
 
   public String get(int index){
     if (index >= 0 && index <= size - 1) return data[index];
     return "Nothing exists at index " + index;
+  }
+
+  public String set(int index, String element){
+    if (index >= 0 && index <= size - 1){
+      String temp = data[index];
+      data[index] = element;
+      return temp;
+    }
+    return "Nothing exists at index " + index;
+  }
+
+  public void resize(){
+    String[] newArray = new String[data.length + 1];
+    for (int start = 0; start < size; start++){
+      newArray[start] = get(start);
+    }
+    data = newArray;
   }
 
 }
--- SuperArrayTester.java
+++ SuperArrayTester.java
@@ -10,6 +10,45 @@ public class SuperArrayTester{
     System.out.println(a1.size()); // Should print 1
     System.out.println(a1.get(0)); // Should print "One"
     System.out.println(a1.get(1)); // Should print "Nothing exists at index 1"
+    System.out.println(a1.set(0, "Zero")); // Should print "One"
+    System.out.println(a1.get(0)); // Should print "Zero"
+    System.out.println(a1.get(1)); // Should print "Nothing exists at index 1"
+    System.out.println(Arrays.toString(a1.data));
+    a1.resize();
+    System.out.println(Arrays.toString(a1.data));
+    a1.add("One");
+    a1.add("Two");
+    System.out.println(a1.size()); //Should print 3
+    a1.resize();
+    System.out.println(Arrays.toString(a1.data));
+
+    SuperArray a2 = new SuperArray();
+    System.out.println(Arrays.toString(a2.data));
+    a2.add("Test");
+    a2.add("Test");
+    a2.add("Test");
+    a2.add("Test");
+    a2.add("Test");
+    a2.add("Test");
+    a2.add("Test");
+    a2.add("Test");
+    a2.add("Test");
+    a2.add("Test");
+    System.out.println(Arrays.toString(a2.data));
+    System.out.println(a2.size()); // Should print 10
+    a2.add("HA");
+    System.out.println(Arrays.toString(a2.data));
+    System.out.println(a2.size()); // Should print 11
+
+    SuperArray words = new SuperArray();
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    for(int i = 0; i < words.size(); i++){
+      System.out.println( words.get(i) );
+    }
+    System.out.println(words.size()); // Should print 3
+    System.out.println(Arrays.toString(words.data));
 
   }
 
